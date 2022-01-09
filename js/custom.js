54eea7e7b2e1a03341e2858d904066dcd1ec8760
Zach <tremblay.zak@gmail.com>
2022-01-08 21:44:55 -0500

    finished loader and connected js file to HTML

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,7 @@
-var loader = document.getElementById("loader-ripple");
-window.onload = function() {
-    loader.style.display = 'none';
-}
+$(window).on('load', function(){
+    $("#loader-ripple").fadeOut(1000);
+    $('body').delay(1000).queue(function(next){
+        $(this).addClass('loaded');
+    });
+    openPage(1);
+});
